cc6ecd806951b734eea8069cca60326fe3e8b8c4
Saurabh <sauravthakulla683@gmail.com>
2025-11-05 23:42:56 +0530

    sigin created

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const auth = require("./routes/auth");
 require("./conn/conn");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use("/api/v1", auth);
 
 app.listen(1000, (e) => {
   console.log(`server started at ${e}`);
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,6 +4,10 @@ const User = require("../models/user");
 //Sign IN
 router.post("/register", async (req, res) => {
   try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username, password });
+    await user.save();
+    res.status(200).json({ user: user });
   } catch (error) {
     res.send("Error");
     console.log("Error");
