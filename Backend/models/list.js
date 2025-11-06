e8388cd0e4575ca782c11362042b0e95e6043655
Saurabh <sauravthakulla683@gmail.com>
2025-11-07 00:40:03 +0530

    Backend completed

--- Backend/models/list.js
+++ Backend/models/list.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
-const listSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const listSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User", 
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("List", listSchema);
--- Backend/routes/list.js
+++ Backend/routes/list.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const List = require("../models/list");
+const user = require("../models/user");
 
 router.post("/addtask", async (req, res) => {
   try {
@@ -50,18 +51,41 @@ router.put("/updatetask/:id", async (req, res) => {
 router.delete("/delete/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const removetask = await User.findOneAndUpdate(
-      { email },
-      { $pull: { list: req.params.id } },
-      { new: true }
-    );
     const delteTasks = await List.findByIdAndDelete(id);
     if (!delteTasks) {
       return res.status(404).json({ message: "Task not found" });
     }
+    const removetask = await User.findByIdAndUpdate(delteTasks.user, {
+      $pull: { list: delteTasks._id },
+    });
+
     res.status(200).json({ message: "Delete Tasked succedfully" });
   } catch (error) {
     console.log("Error in list router:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// get tasks
+router.get("/gettasks", async (req, res) => {
+  try {
+    const { email } = req.query;
+
+    const user = await User.findOne({ email }).populate({
+      path: "list",
+      options: { sort: { createdAt: -1 } }, // 👈 newest first
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({
+      message: "Tasks fetched successfully",
+      tasks: user.list,
+    });
+  } catch (error) {
+    console.log("Error in gettasks:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
